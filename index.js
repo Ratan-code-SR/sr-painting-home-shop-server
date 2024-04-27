759d9ee1573cf45cd07948a1569ff94b0862c946
ratan <ratanchakma68@gmail.com>
2024-04-28 01:01:29 +0600

    email get post updated

--- index.js
+++ index.js
@@ -34,19 +34,24 @@ async function run() {
             res.send(result)
         })
 
-        app.get("/items/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const user = await productCollection.findOne(query)
-            res.send(user)
 
-        })
+        
         app.get("/items/:email", async (req, res) => {
             const email = req.params.email;
             const query = { email: email }
             const cursor = productCollection.find(query);
             const result = await cursor.toArray()
             res.send(result)
+        })
+
+
+
+        app.get("/items/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const user = await productCollection.findOne(query)
+            res.send(user)
+
         })
 
         app.delete("/items/:id", async (req, res) => {
